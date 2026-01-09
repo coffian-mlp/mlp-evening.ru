7aafd1736adfdc2937c19699b9a9e50bf6d9a86a
coffian-mlp <coffian7@gmail.com>
2026-01-09 12:45:50 +0300

    Изменение отрисовки виджета телеграма 3 -_-'

--- index.php
+++ index.php
@@ -355,16 +355,7 @@ require_once __DIR__ . '/src/templates/header.php';
                             <img src="https://telegram.org/favicon.ico" width="20"> Telegram
                         </span>
                         <div id="telegram-bind-container" style="min-height: 40px; display: flex; align-items: center;">
-                            <!-- Виджет загружается сразу, JS скроет его, если аккаунт уже привязан -->
-                            <div id="telegram-widget-wrapper">
-                                <script async src="https://telegram.org/js/telegram-widget.js?22" 
-                                        data-telegram-login="<?= htmlspecialchars($telegramBotUsername) ?>" 
-                                        data-size="small" 
-                                        data-radius="5" 
-                                        data-onauth="onTelegramAuth(user)" 
-                                        data-request-access="write"></script>
-                            </div>
-                            <span id="telegram-status-text" style="display:none; color: green; font-weight: bold;"></span>
+                            <small class="loading-text">Загрузка...</small>
                         </div>
                     </div>
                 </div>
